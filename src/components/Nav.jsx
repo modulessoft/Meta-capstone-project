73d0daadd7d1ba06df4251a6f771f2e44d39a5af
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-16 10:19:16 -0700

    Add navbar items

---
+++ src/components/Nav.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <img src="../assets/icons_assets/Logo.svg" alt="logo" />
+        </li>
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/menu">Menu</a>
+        </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="/order-online">Order Online</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
