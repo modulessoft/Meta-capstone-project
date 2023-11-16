b79d2faaa650b902b759c8dc7e5d7e2467142369
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-16 12:15:52 -0700

    Add and extract layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,56 +2,50 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer style={{ display: "flex", flexDirection: "column" }}>
+    <footer>
       <ul>
         <li>
-          <img src="" alt="" />
+          <img src="../assets/icons_assets/Logo.svg" alt="" />
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <strong>Doormat Navigation</strong>
         </li>
         <li>
-          <ul>
-            <li>
-              <strong>Doormat Navigation</strong>
-            </li>
-            <li>
-              <a href="/">Home</a>
-            </li>
-            <li>
-              <a href="/about">About</a>
-            </li>
-            <li>
-              <a href="/menu">Menu</a>
-            </li>
-            <li>
-              <a href="/reservations">Reservations</a>
-            </li>
-            <li>
-              <a href="/order-online">Order Online</a>
-            </li>
-            <li>
-              <a href="/login">Login</a>
-            </li>
-          </ul>
+          <a href="/">Home</a>
         </li>
         <li>
-          <ul>
-            <li>
-              <strong>Contact</strong>
-            </li>
-            <li>Address</li>
-            <li>Phone Number</li>
-            <li>E-mail</li>
-          </ul>
+          <a href="/about">About</a>
         </li>
         <li>
-          <ul>
-            <li>
-              <strong>Social Media Links</strong>
-            </li>
-            <li>Address</li>
-            <li>Phone Number</li>
-            <li>E-mail</li>
-          </ul>
+          <a href="/menu">Menu</a>
         </li>
+        <li>
+          <a href="/reservations">Reservations</a>
+        </li>
+        <li>
+          <a href="/order-online">Order Online</a>
+        </li>
+        <li>
+          <a href="/login">Login</a>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <strong>Contact</strong>
+        </li>
+        <li>Address</li>
+        <li>Phone Number</li>
+        <li>E-mail</li>
+      </ul>
+      <ul>
+        <li>
+          <strong>Social Media Links</strong>
+        </li>
+        <li>Address</li>
+        <li>Phone Number</li>
+        <li>E-mail</li>
       </ul>
     </footer>
   );
