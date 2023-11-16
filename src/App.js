809cd958d604555893c334df0e1f8a803099f4c8
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-15 19:02:46 -0700

    Break the sections down into React components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import logo from "./logo.svg";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <header></header>
-      <nav></nav>
-      <main></main>
-      <footer></footer>
+      <Header></Header>
+      <Nav></Nav>
+      <Main></Main>
+      <Footer></Footer>
     </>
   );
 }
