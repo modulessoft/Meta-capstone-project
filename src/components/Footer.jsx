b5e0548c086b091d4d293b0d293179b401344534
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-16 10:25:50 -0700

    Add footer elements structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,60 @@
 import React from "react";
 
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <footer style={{ display: "flex", flexDirection: "column" }}>
+      <ul>
+        <li>
+          <img src="" alt="" />
+        </li>
+        <li>
+          <ul>
+            <li>
+              <strong>Doormat Navigation</strong>
+            </li>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/about">About</a>
+            </li>
+            <li>
+              <a href="/menu">Menu</a>
+            </li>
+            <li>
+              <a href="/reservations">Reservations</a>
+            </li>
+            <li>
+              <a href="/order-online">Order Online</a>
+            </li>
+            <li>
+              <a href="/login">Login</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <ul>
+            <li>
+              <strong>Contact</strong>
+            </li>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>E-mail</li>
+          </ul>
+        </li>
+        <li>
+          <ul>
+            <li>
+              <strong>Social Media Links</strong>
+            </li>
+            <li>Address</li>
+            <li>Phone Number</li>
+            <li>E-mail</li>
+          </ul>
+        </li>
+      </ul>
+    </footer>
+  );
 }
 
 export default Footer;
