879e342b6e170e9239fa88d3e5b5e90cb4492e4c
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-18 21:57:31 -0700

    Extract 404 page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home/index";
 import BookingPage from "./components/Booking/BookingPage";
+import NotFound from "./components/404/index.jsx";
 import "./App.css";
 
 function App() {
@@ -10,18 +11,7 @@ function App() {
         <Route path="/">
           <Route index element={<Home />} />
           <Route path="booking" element={<BookingPage />} />
-          <Route
-            path="*"
-            element={
-              <div style={{ textAlign: "center", padding: "50px" }}>
-                <h1 style={{ color: "#dc3545" }}>404 - Page Not Found</h1>
-                <p>
-                  Sorry, the page you are looking for might be in another
-                  castle.
-                </p>
-              </div>
-            }
-          />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
---
+++ src/components/404/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function index() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1 style={{ color: "#dc3545" }}>404 - Page Not Found</h1>
+      <p>Sorry, the page you are looking for might be in another castle.</p>
+    </div>
+  );
+}
+
+export default index;
