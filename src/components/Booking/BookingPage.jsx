b49648a16314c3c82a33a09e77d87a59e7ecced2
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-19 10:41:26 -0700

    Move Nav and Header componenets to a different directory

--- src/components/Booking/BookingPage.jsx
+++ src/components/Booking/BookingPage.jsx
@@ -1,15 +1,21 @@
 import React from "react";
 import BookingForm from "./BookingForm";
+import Header from "../Header/Header";
+import Nav from "../Header/Nav";
 
 function BookingPage() {
   return (
-    <div className="booking-page-container">
-      <div className="booking-page-content">
-        <h2>Reservation Form</h2>
-        <p>Fill out the form below to make your reservation.</p>
-        <BookingForm />
+    <>
+      <Header></Header>
+      <Nav></Nav>
+      <div className="booking-page-container">
+        <div className="booking-page-content">
+          <h2>Reservation Form</h2>
+          <p>Fill out the form below to make your reservation.</p>
+          <BookingForm />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import Header from "./Header";
-import Nav from "./Nav";
+import Header from "../Header/Header";
+import Nav from "../Header/Nav";
 import Main from "./Main";
 import Footer from "./Footer";
 function index() {
