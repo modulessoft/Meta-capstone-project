2959d1370af9cecfcfdde9f4eba43926ed20c409
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-18 22:01:10 -0700

    Change the reservation button to a real link

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -1,18 +1,21 @@
 import React from "react";
 import image from "../../assets/icons_assets/restaurant.jpg";
+import { Link } from "react-router-dom";
 function Hero() {
   return (
     <section className="hero">
       <div className="px-2 mb-2">
         <h1>Little Lemon</h1>
         <h4>Chicago</h4>
-        <p>
+        <p className="mb-2">
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis sed
           rerum modi eveniet, doloremque alias libero similique eius. Nulla
           tenetur dolores sequi assumenda quaerat molestiae hic similique
           aperiam! Ipsam, ratione.
         </p>
-        <button className="btn">Reserve a Table</button>
+        <Link to="/Booking" className="btn">
+          Reserve a Table
+        </Link>
       </div>
       <div className="px-2">
         <img src={image} alt="restaurant" width="100%" />
