0a5aa908d45678e8c2fdc786861d80bb63ca660b
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-19 10:52:21 -0700

    Add state for form fields

--- src/components/Booking/BookingForm.jsx
+++ src/components/Booking/BookingForm.jsx
@@ -1,23 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 function BookingForm() {
+  const [date, setDate] = useState();
+  const [time, setTime] = useState();
+  const [guests, setGuests] = useState();
+  const [occasion, setOccasion] = useState();
+  const [availableTimes, setAvailableTimes] = useState([
+    "17:00",
+    "18:00",
+    "19:00",
+    "20:00",
+    "21:00",
+    "22:00",
+  ]);
   return (
     <form className="booking-form">
       <label htmlFor="res-date">Choose date</label>
-      <input type="date" id="res-date" />
+      <input
+        type="date"
+        id="res-date"
+        onChange={(e) => {
+          setDate(e.target.value);
+        }}
+      />
       <label htmlFor="res-time">Choose time</label>
-      <select id="res-time">
-        <option>17:00</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-        <option>21:00</option>
-        <option>22:00</option>
+      <select
+        id="res-time"
+        onChange={(e) => {
+          setTime(e.target.value);
+        }}
+      >
+        {availableTimes.map((time, key) => (
+          <option key={key}>{time}</option>
+        ))}
       </select>
       <label htmlFor="guests">Number of guests</label>
-      <input type="number" placeholder="1" min="1" max="10" id="guests" />
+      <input
+        type="number"
+        placeholder="1"
+        min="1"
+        max="10"
+        id="guests"
+        onChange={(e) => {
+          setGuests(e.target.value);
+        }}
+      />
       <label htmlFor="occasion">Occasion</label>
-      <select id="occasion">
+      <select
+        id="occasion"
+        onChange={(e) => {
+          setOccasion(e.target.value);
+        }}
+      >
         <option>Birthday</option>
         <option>Anniversary</option>
       </select>
