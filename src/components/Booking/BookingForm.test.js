b409e01c44697be133a26b6f13b02b0f6367bb5f
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-19 17:40:53 -0700

    Add test for the header text

---
+++ src/components/Booking/BookingForm.test.js
@@ -0,0 +1,9 @@
+import { render, screen } from "@testing-library/react";
+import BookingForm from "./BookingForm";
+
+test("Renders the BookingForm heading", () => {
+  const { getByText } = render(
+    <BookingForm availableTimes={["10:00"]} dispatchAvailableTimes={() => {}} />
+  );
+  expect(getByText("Reservation Form")).toBeInTheDocument();
+});
