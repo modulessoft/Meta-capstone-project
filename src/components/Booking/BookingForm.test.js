e169c81eb97fc1709bae2411b865d89e4b77b2d3
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-20 21:58:15 -0700

    Add unit test for form validation

--- src/components/Booking/BookingForm.test.js
+++ src/components/Booking/BookingForm.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
 import BookingForm from "./BookingForm";
 
 test("Renders the BookingForm heading", () => {
@@ -6,4 +6,28 @@ test("Renders the BookingForm heading", () => {
     <BookingForm availableTimes={["10:00"]} dispatchAvailableTimes={() => {}} />
   );
   expect(getByText("Choose date")).toBeInTheDocument();
+});
+
+describe("BookingForm", () => {
+  it("should display error messages for invalid form submission", async () => {
+    render(
+      <BookingForm
+        availableTimes={["10:00"]}
+        dispatchAvailableTimes={() => {}}
+      />
+    );
+
+    const submitButton = screen.getByText("Make Your reservation");
+
+    fireEvent.click(submitButton);
+
+    // Validate that error messages are displayed
+    const errorMessages = await screen.findAllByRole("alert");
+    expect(errorMessages).toHaveLength(3);
+    expect(
+      screen.getByText("Please add more than 0 guests")
+    ).toBeInTheDocument();
+    expect(screen.getByText("Please add a correct time")).toBeInTheDocument();
+    expect(screen.getByText("Please add a correct date")).toBeInTheDocument();
+  });
 });
