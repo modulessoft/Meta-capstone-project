c7fc80d7577ae313e19887c9fdb91996b501e174
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-20 22:23:11 -0700

    Add unit test for form validation when it is valid

--- src/components/Booking/BookingForm.test.js
+++ src/components/Booking/BookingForm.test.js
@@ -30,4 +30,39 @@ describe("BookingForm", () => {
     expect(screen.getByText("Please add a correct time")).toBeInTheDocument();
     expect(screen.getByText("Please add a correct date")).toBeInTheDocument();
   });
+
+  it("should submit the form for valid input", async () => {
+    const mockSubmitData = jest.fn();
+    render(
+      <BookingForm
+        availableTimes={["12:00"]}
+        dispatchAvailableTimes={() => {}}
+        submitData={mockSubmitData}
+      />
+    );
+
+    // Provide valid input
+    fireEvent.change(screen.getByLabelText("Choose date"), {
+      target: { value: "2023-11-23" },
+    });
+    fireEvent.change(screen.getByLabelText("Choose time"), {
+      target: { value: "12:00" },
+    });
+    fireEvent.change(screen.getByLabelText("Number of guests"), {
+      target: { value: "2" },
+    });
+    fireEvent.change(screen.getByLabelText("Occasion"), {
+      target: { value: "Birthday" },
+    });
+
+    fireEvent.click(screen.getByText("Make Your reservation"));
+
+    // Validate that the form is submitted
+    expect(mockSubmitData).toHaveBeenCalledWith({
+      date: "2023-11-23",
+      time: "12:00",
+      numberOfGuests: "2",
+      occasion: "Birthday",
+    });
+  });
 });
