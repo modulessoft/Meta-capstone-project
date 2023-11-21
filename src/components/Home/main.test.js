79209fee04cfcb1a33632b4efd0a1083eaae90f6
Yousef Fatouraee <yousef@Yousefs-MacBook-Pro.local>
2023-11-20 19:20:11 -0700

    Add test for updated times

--- src/components/Home/main.test.js
+++ src/components/Home/main.test.js
@@ -14,3 +14,23 @@ test("Should have available booking time options", async () => {
   const options = await screen.getByLabelText("Choose time");
   expect(options.length).toBeGreaterThan(0);
 });
+
+test("Should update available booking time options when changing booking date", async () => {
+  render(
+    <MemoryRouter>
+      <Main />
+    </MemoryRouter>
+  );
+  const exampleDate = "2023-11-24";
+  // open the reservation modal
+  const btn = screen.getByText("Reserve a Table");
+  fireEvent.click(btn);
+  // get initial value
+  const initialOptions = await screen.getByLabelText("Choose time");
+  const dateInput = await screen.getByLabelText("Choose date");
+  fireEvent.change(dateInput, { target: { value: exampleDate } });
+  fireEvent.blur(dateInput);
+  expect(dateInput).toHaveValue(exampleDate);
+  const newOptions = await screen.getByLabelText("Choose time");
+  expect(initialOptions.length).not.toEqual(newOptions.length);
+});
